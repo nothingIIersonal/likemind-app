b07639a99d3be27a2d63bc3b4690a5181792c073
nothingIIersonal <velochimc@mail.ru>
2024-01-16 18:47:00 +0300

    Add full profile data to web-view

--- web-view/src/pages/Profile.jsx
+++ web-view/src/pages/Profile.jsx
@@ -6,6 +6,11 @@ import { AuthCheck } from "../config/AuthCheck";
 function Profile() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
+  const [firstname, setFirstname] = useState("");
+  const [about, setAbout] = useState("");
+  const [sex, setSex] = useState("");
+  const [createdAt, setCreatedAt] = useState("");
+  const [updatedAt, setUpdatedAt] = useState("");
 
   const navigate = useNavigate();
 
@@ -14,6 +19,11 @@ function Profile() {
       .then((resp) => {
         setUsername(resp.data.username);
         setEmail(resp.data.email);
+        setFirstname(resp.data.firstname ? resp.data.firstname : "-");
+        setAbout(resp.data.about ? resp.data.about : "-");
+        setSex(resp.data.sex ? resp.data.sex : "-");
+        setCreatedAt(new Date(resp.data.created_at).toLocaleDateString());
+        setUpdatedAt(new Date(resp.data.updated_at).toLocaleDateString());
       })
       .catch((err) => {
         console.log("Error occured");
@@ -46,11 +56,23 @@ function Profile() {
               <span className="firstname">
                 <p>First name:</p>
               </span>
-              <span className="firstname-val">{email}</span>
+              <span className="firstname-val">{firstname}</span>
               <span className="about">
                 <p>About:</p>
               </span>
-              <span className="about-val">{email}</span>
+              <span className="about-val">{about}</span>
+              <span className="sex">
+                <p>Sex:</p>
+              </span>
+              <span className="sex-val">{sex}</span>
+              <span className="created-at">
+                <p>Profile created:</p>
+              </span>
+              <span className="created-at-val">{createdAt}</span>
+              <span className="updated-at">
+                <p>Profile updated:</p>
+              </span>
+              <span className="updated-at-val">{updatedAt}</span>
             </div>
             <div className="profile-buttons">
               <span className="logout-btn">
