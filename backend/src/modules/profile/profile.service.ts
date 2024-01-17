d64e03f1988ac1c4670150dfdb62b658fd8432eb
nothingIIersonal <velochimc@mail.ru>
2024-01-18 02:55:16 +0300

    Remove unused function

--- backend/src/modules/profile/profile.service.ts
+++ backend/src/modules/profile/profile.service.ts
@@ -8,30 +8,6 @@ import User from '@models/user.model';
 
 @Injectable()
 export class ProfileService {
-  async profileUser(username: string, email: string, password: string) {
-    if (username.length == 0 || email.length == 0 || password.length == 0) {
-      throw new BadRequestException('Fill all fields');
-    }
-
-    const emailExists = await UserRepository.existsByEmail(email);
-    const usernameExists = await UserRepository.existsByUsername(username);
-    if (emailExists || usernameExists) {
-      throw new BadRequestException('User not found or invalid data');
-    }
-
-    let user: User;
-    user.username = username;
-    user.email = email;
-    user.password = password;
-
-    const response = await UserRepository.save(user);
-    if (!response) {
-      throw new InternalServerErrorException('Unknown error');
-    }
-
-    return { status: 200, message: 'Successfully profileed' };
-  }
-
   async updateProfile(user: User) {
     const data = await UserRepository.updateProfile(user)
       .then((res) => {
